28f0a89fcfd4a693ccabbc25d0a0a6ed324059b8
yifeiy3 <47006864+yifeiy3@users.noreply.github.com>
2020-04-05 19:37:44 -0700

    Added proper time handling

--- getlog.py
+++ getlog.py
@@ -5,8 +5,8 @@ API_KEY = "ff5c476f-1b99-4fc7-a747-0bed31268f11"    #The API Key of the monitor
 ENDPOINT = "https://graph.api.smartthings.com/api/smartapps/installations/bb64b684-213a-492e-8138-9b6722e1e9e5"   #The API endpoint of the monitor
 IMPORTANT = ["Virtual Switch 2"]  #important devices to keep track of state changes
 output = 'deviceInfos/alldevicelog.txt' #output folder
-#since = datetime.utcnow() - timedelta(minutes=110) #check interaction since last hour 
-#TODO: Why would this return error when since is not None, figure out time in groovy
+#since = datetime.utcnow() - timedelta(minutes=110) #check interaction since last hour
+
 since = None
 
 An = Analysis(API_KEY, ENDPOINT, important = IMPORTANT)
