88d1645259a09e33b6db3d1491dfe2865e66b28f
yifeiy3 <47006864+yifeiy3@users.noreply.github.com>
2020-04-05 19:38:44 -0700

    Added testing for acquiring events

--- test.py
+++ test.py
@@ -10,7 +10,10 @@ d_info = md.getThings("all")
 m_info = md.getThings("monitor")
 
 monitor_states = md.getStates("lastExec", monitorid) #oka is deviceid for monitor
-monitor_events = md.getEvents(monitorid)
+monitor_events = md.getEvents(monitorid, 100)
+
+switch_states = md.getStates("switch", "abeafef6-7372-4347-bab6-4f485b8fb2d7")
+switch_events = md.getEvents("abeafef6-7372-4347-bab6-4f485b8fb2d7", 5)
 
 with open('deviceInfos/alldevicelogtest.txt', 'w') as outfile:
     outfile.write("All device info: \n")
@@ -21,3 +24,8 @@ with open('deviceInfos/alldevicelogtest.txt', 'w') as outfile:
     json.dump(monitor_states, outfile)
     outfile.write("\nMonitor events: \n")
     json.dump(monitor_events, outfile)
+    outfile.write("\nSwitch states: \n")
+    json.dump(switch_states, outfile)
+    outfile.write("\nSwitch events: \n")
+    json.dump(switch_events, outfile)
+
